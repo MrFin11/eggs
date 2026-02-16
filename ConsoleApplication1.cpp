27a416a88c9fddd16eb1c63efae0bd63045eb708
Dima <dimanshishov2008@gmail.com>
2026-02-16 15:35:27 +0300

    calc

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,7 +1,33 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    cout << "=== КАЛЬКУЛЯТОР v1.0 ===" << endl;
+    cout << "Доступные операции: +, -" << endl;
+
+    int a, b;
+    char op;
+
+    cout << "Введите выражение (например: 5 + 3): ";
+    cin >> a >> op >> b;
+
+    if (op == '+') {
+        cout << "Результат: " << add(a, b) << endl;
+    }
+    else if (op == '-') {
+        cout << "Результат: " << subtract(a, b) << endl;
+    }
+    else {
+        cout << "Ошибка: неподдерживаемая операция!" << endl;
+    }
+
+    return 0;
 }
